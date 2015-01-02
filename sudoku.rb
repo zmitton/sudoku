92a2b9867ebb20de6241eb7d7d016f06a939744a
carolineartz <ceartz@gmail.com>
2015-01-02 13:11:28 -0800

    convert multiline blocks to do..end syntax

--- sudoku.rb
+++ sudoku.rb
@@ -1,87 +1,77 @@
 #require 'pry'
 #require 'pry-nav'
 
-#TEMP SHIT
-def get_solution_string(input_string)
-  board = Sudoku.new(input_string)
-  #board.print_board
-  solved_board = board.solve!
-  board.print_board
-  #puts board.stringify
-  board.stringify
-end
-
 class Sudoku
   attr_accessor :full_board
-   def initialize(board_string)
+  def initialize(board_string)
     @board_string = board_string.to_s.split("")
-    @full_board = (0..2).map { |tri_square|
-      (0..2).map {|square|
-        (0..2).map {|tri_cell|
-          (0..2).map {|cell|
+    @full_board = (0..2).map do |tri_square|
+      (0..2).map do |square|
+        (0..2).map do |tri_cell|
+          (0..2).map do |cell|
             digit = @board_string[tri_square*27 + square*3 + tri_cell*9 +cell]
-           Cell.new(digit, tri_square, square, tri_cell, cell)
-          }
-        }
-      }
-    }
+            Cell.new(digit, tri_square, square, tri_cell, cell)
+          end
+        end
+      end
+    end
   end
 
   def loop_square(tri_square, square)
-    (0..2).each{|tri_cell|
-      (0..2).each{|cell|
+    (0..2).each do |tri_cell|
+      (0..2).each do |cell|
         yield @full_board[tri_square][square][tri_cell][cell]
-      }
-    }
+      end
+    end
   end
 
   def loop_row(tri_square, tri_cell)
-    (0..2).each{|square|
-      (0..2).each{|cell|
-         yield @full_board[tri_square][square][tri_cell][cell]
-      }
-    }
+    (0..2).each do |square|
+      (0..2).each do |cell|
+        yield @full_board[tri_square][square][tri_cell][cell]
+      end
+    end
   end
 
   def loop_column(square, cell)
-    (0..2).each{|tri_square|
-      (0..2).each{|tri_cell|
-         yield @full_board[tri_square][square][tri_cell][cell]
-      }
-    }
+    (0..2).each do |tri_square|
+      (0..2).each do |tri_cell|
+        yield @full_board[tri_square][square][tri_cell][cell]
+      end
+    end
   end
 
   def loop_board
-    (0..2).each{|tri_square|
-      (0..2).each{|tri_cell|
-        loop_row(tri_square,tri_cell){|cell|yield cell}
-      }
-    }
+    (0..2).each do |tri_square|
+      (0..2).each do |tri_cell|
+        loop_row(tri_square, tri_cell) { |cell| yield cell }
+      end
+    end
   end
 
   def solve!
 
-   until finished?
-    begin
-      something_changed = false
-      something_changed = standard_method || elimination_method
-    end while something_changed
+    until finished?
+      begin
+        something_changed = false
+        something_changed = standard_method || elimination_method
+      end while something_changed
 
 
-    if has_mistakes?
-      return false
-    elsif !finished?
-      return guessing_method
+      if has_mistakes?
+        return false
+      elsif !finished?
+        return guessing_method
+      end
     end
-   end
-   return self
+    return self
   end
 
   def has_mistakes?
     mistakes = false
-    loop_board{|cell|
+    loop_board do |cell|
       mistakes = true if cell.solution == " " && cell.possibilities == []
-    }
+    end
     #puts "has mistakes: #{mistakes}"
 
     mistakes
@@ -97,98 +87,97 @@ class Sudoku
   end
 
 
-def standard_method
-  begin
-    something_changed = false
-    loop_board do |cell|
-      to_delete=[]
-      cell.possibilities.each do |possibility|
-        loop_row(cell.tri_square_index, cell.tri_cell_index) do |row_cell|
+  def standard_method
+    begin
+      something_changed = false
+      loop_board do |cell|
+        to_delete=[]
+        cell.possibilities.each do |possibility|
+          loop_row(cell.tri_square_index, cell.tri_cell_index) do |row_cell|
             if row_cell.solution == possibility
               to_delete << possibility
               something_changed = true
             end
-        end
-        loop_column(cell.square_index, cell.cell_index) do |column_cell|
-              if column_cell.solution == possibility
-                to_delete << possibility
-                something_changed = true
+          end
+          loop_column(cell.square_index, cell.cell_index) do |column_cell|
+            if column_cell.solution == possibility
+              to_delete << possibility
+              something_changed = true
             end
-        end
-        loop_square(cell.tri_square_index, cell.square_index) do |square_cell|
-              if square_cell.solution == possibility
-                to_delete << possibility
-                something_changed = true
+          end
+          loop_square(cell.tri_square_index, cell.square_index) do |square_cell|
+            if square_cell.solution == possibility
+              to_delete << possibility
+              something_changed = true
             end
+          end
+        end
+        to_delete.each { |non_possibility| cell.possibilities.delete(non_possibility) }
+        if cell.possibilities.length == 1
+          cell.solution = cell.possibilities[0]
+          cell.possibilities = []
         end
       end
-      to_delete.each{|non_possibility| cell.possibilities.delete(non_possibility)}
-      if cell.possibilities.length == 1
-        cell.solution = cell.possibilities[0]
-        cell.possibilities = []
-      end
-    end
-  end while something_changed
-  return something_changed
-end
+    end while something_changed
+    return something_changed
+  end
 
 
   def elimination_method
 
-    #begin
-      something_changed = false
-      loop_board { |cell|
-          cell.possibilities.each { |possibility|
+    something_changed = false
+    loop_board do |cell|
+      cell.possibilities.each do |possibility|
 
 
-            elimination_method_valid = true
- 
+        elimination_method_valid = true
 
 
-            loop_row(cell.tri_square_index, cell.tri_cell_index) { |row_cell|
-              unless row_cell === cell 
-                if row_cell.possibilities.include? possibility            
-                  elimination_method_valid = false
-                end
-              end
-            }
-            if elimination_method_valid
-              cell.solution = possibility
-              cell.possibilities = [possibility]
-              something_changed = true
+
+        loop_row(cell.tri_square_index, cell.tri_cell_index) do |row_cell|
+          unless row_cell === cell
+            if row_cell.possibilities.include? possibility
+              elimination_method_valid = false
             end
-            elimination_method_valid = true
-            loop_column(cell.square_index, cell.cell_index) { |row_cell|
-              unless row_cell === cell 
-                if row_cell.possibilities.include? possibility            
-                  elimination_method_valid = false
-                end
-              end
-            }
-            if elimination_method_valid
-              cell.solution = possibility
-              cell.possibilities = [possibility]
-              something_changed = true
+          end
+        end
+        if elimination_method_valid
+          cell.solution = possibility
+          cell.possibilities = [possibility]
+          something_changed = true
+        end
+        elimination_method_valid = true
+        loop_column(cell.square_index, cell.cell_index) do |row_cell|
+          unless row_cell === cell
+            if row_cell.possibilities.include? possibility
+              elimination_method_valid = false
             end
-            elimination_method_valid = true
-            loop_square(cell.tri_square_index, cell.square_index) { |row_cell|
-              unless row_cell === cell 
-                if row_cell.possibilities.include? possibility            
-                  elimination_method_valid = false
-                end
-              end
-            }
-            if elimination_method_valid
-              cell.solution = possibility
-              cell.possibilities = [possibility]
-              something_changed = true
+          end
+        end
+        if elimination_method_valid
+          cell.solution = possibility
+          cell.possibilities = [possibility]
+          something_changed = true
+        end
+        elimination_method_valid = true
+        loop_square(cell.tri_square_index, cell.square_index) do |row_cell|
+          unless row_cell === cell
+            if row_cell.possibilities.include? possibility
+              elimination_method_valid = false
             end
+          end
+        end
+        if elimination_method_valid
+          cell.solution = possibility
+          cell.possibilities = [possibility]
+          something_changed = true
+        end
 
-          }
+      end
 
 
-           # cell.solution = cell.possibilities[0] if cell.possibilities.length == 1
-        }
+      # cell.solution = cell.possibilities[0] if cell.possibilities.length == 1
+    end
 
 
 
@@ -201,46 +190,44 @@ end
 
 
   def guessing_method
-        #puts "hello"
+    #puts "hello"
 
-        smallest_num_of_possibilities = 9
-        loop_board{|cell|
-          smallest_num_of_possibilities = cell.possibilities.length if cell.possibilities.length < smallest_num_of_possibilities && cell.solution == " "
-          #puts "#{smallest_num_of_possibilities} , #{cell.inspect}"
-        }
+    smallest_num_of_possibilities = 9
+    loop_board do |cell|
+      smallest_num_of_possibilities = cell.possibilities.length if cell.possibilities.length < smallest_num_of_possibilities && cell.solution == " "
+      #puts "#{smallest_num_of_possibilities} , #{cell.inspect}"
+    end
 
-        loop_board{|cell|
-          if cell.possibilities.length == smallest_num_of_possibilities
-            cell.possibilities.each_with_index{|guess_value, guess_index|
+    loop_board do |cell|
+      if cell.possibilities.length == smallest_num_of_possibilities
+        cell.possibilities.each_with_index do |guess_value, guess_index|
 
-             guessing_board_copy = Marshal.load(Marshal.dump(self))
-             guessing_board_copy.full_board[cell.tri_square_index][cell.square_index][cell.tri_cell_index][cell.cell_index].solution = guess_value
-             guessing_board_copy.full_board[cell.tri_square_index][cell.square_index][cell.tri_cell_index][cell.cell_index].possibilities = []
-             #puts "here5 #{guessing_board_copy.full_board[cell.tri_square_index][cell.square_index][cell.tri_cell_index][cell.cell_index].inspect}"
+          guessing_board_copy = Marshal.load(Marshal.dump(self))
+          guessing_board_copy.full_board[cell.tri_square_index][cell.square_index][cell.tri_cell_index][cell.cell_index].solution = guess_value
+          guessing_board_copy.full_board[cell.tri_square_index][cell.square_index][cell.tri_cell_index][cell.cell_index].possibilities = []
+          #puts "here5 #{guessing_board_copy.full_board[cell.tri_square_index][cell.square_index][cell.tri_cell_index][cell.cell_index].inspect}"
 
-            if guessing_board_copy.solve!
+          if guessing_board_copy.solve!
 
-              # puts guessing_board_copy.print_board
-              self.full_board = guessing_board_copy.full_board
-              return guessing_board_copy
-            end 
-
-              }
-
-
-
-
-            # if !guessing_board_copy.solve!
-            #   puts guessing_board_copy.print_board
-            #   break
-            # else
-            #   puts guessing_board_copy.print_board
-            # end       
-             return false #guessing_board_copy.solve!
-
-            # return guessing_board_copy if !has_mistakes?
+            # puts guessing_board_copy.print_board
+            self.full_board = guessing_board_copy.full_board
+            return guessing_board_copy
           end
-        }
+
+        end
+
+
+        # if !guessing_board_copy.solve!
+        #   puts guessing_board_copy.print_board
+        #   break
+        # else
+        #   puts guessing_board_copy.print_board
+        # end
+        return false #guessing_board_copy.solve!
+
+        # return guessing_board_copy if !has_mistakes?
+      end
+    end
 
     #only make one guess and then continue the normal way (and guess on one where the possibilities are only 2)
     #find a way to save state before the guess.
@@ -271,8 +258,6 @@ end
     end
     output_string
   end
-
-
 
 end
 
@@ -312,6 +297,3 @@ board = Sudoku.new(input_string)
 solved_board = board.solve!
 board.print_board
 board.stringify
-
-
-
