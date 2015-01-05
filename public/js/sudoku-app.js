68d42ba35d07ffbc3509ef36e03072a6f946abd8
carolineartz <ceartz@gmail.com>
2015-01-04 23:43:02 -0600

    angular setup

--- public/js/sudoku-app.js
+++ public/js/sudoku-app.js
@@ -1,14 +1,8 @@
 var app = angular.module('sudoku', []);
 
-var cellValues = Array.apply(null, Array(81)).map(function() { return "" });
-
 app.controller('SudokuCtrl', function($scope, $http) {
-  var cells = [];
-   for (var i = 0; i < cellValues.length; i++ ) {
-        if (i % 9 == 0) cells.push([]);
-        cells[cells.length-1].push(cellValues[i]);
-    }
-  return $scope.cells = cells;
+  $scope.cellColor = "";
+
 });
 
 
