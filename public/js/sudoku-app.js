e40105085462f3181f805e518681a1da69483113
carolineartz <ceartz@gmail.com>
2015-01-04 22:56:31 -0600

    cleanup javascript

--- public/js/sudoku-app.js
+++ public/js/sudoku-app.js
@@ -1,1 +1,18 @@
 var app = angular.module('sudoku', []);
+
+var cellValues = Array.apply(null, Array(81)).map(function() { return "" });
+
+app.controller('SudokuCtrl', function($scope, $http) {
+  var cells = [];
+   for (var i = 0; i < cellValues.length; i++ ) {
+        if (i % 9 == 0) cells.push([]);
+        cells[cells.length-1].push(cellValues[i]);
+    }
+  return $scope.cells = cells;
+});
+
+
+
+
+    
+
--- public/js/sudoku.js
+++ public/js/sudoku.js
@@ -1,64 +1,10 @@
-var board = "g";
-
 window.onload = function() {
-    $(".table_cell").each(function() {
-        console.log(this);
-        input_field = document.createElement("input");
-        input_field.setAttribute("type", "text");
-        input_field.setAttribute("maxlength", "1");
-        input_field.setAttribute("size", "1");
-        input_field.onfocus = function() {
-            this.select();
-        }
-        input_field.onkeyup = board_changed;
-        this.appendChild(input_field);
-
-    });
     if (document.getElementById("solution_string")) {
         fill_board_with_solution();
     }
-
 }
-
-function x() {
-    for (i = 0; i <= 1000000000; i++) {
-        z = 1 + 1;
-    }
-    //console.log("hello");
-}
-
-function y() {
-    //console.log("hello2");
-
-}
-
-function board_changed() {
-    //console.log("board changed");
-    board = map_board_from_UI();
-    board.run_solver();
-    board.print_result();
-    //console.log(this.parentNode.id);
-    var next_cell = parseInt(this.parentNode.id) + 1;
-    if (next_cell <= 81) {
-        //console.log(next_cell);
-        document.getElementById(next_cell).childNodes[0].select();
-    }
-}
-
-function map_board_from_UI() {
-    //console.log("map_board_from_UI");
-    return new Board();
-}
-
 
 function Board() {
-    this.run_solver = function() {
-        //console.log("run_solver");
-    }
-    this.print_result = function() {
-        //console.log("print_result");
-    }
-
     this.tri_square_arr = [];
     for (tri_square = 0; tri_square <= 2; tri_square++) {
         var square_arr = [];
@@ -80,15 +26,14 @@ function Board() {
 
 
 function go() {
-    var url = "/"
     var string = make_string();
-    window.location.href = url + string;
+    window.location.href = "/" + string;
 }
 
 function make_string() {
     user_string = ""
     for (i = 1; i <= 81; i++) {
-        cell_value = document.getElementById(i).childNodes[0].value
+        cell_value = $('#' + i + ' input').val();
         if (cell_value == "")
             cell_value = "0";
         user_string += cell_value;
@@ -100,8 +45,9 @@ function make_string() {
 
 function fill_board_with_solution() {
     for (i = 1; i <= 81; i++) {
-        cell = document.getElementById(i).childNodes[0]
-        cell.value = document.getElementById("solution_string").innerHTML.charAt(i - 1);
-        console.log(document.getElementById("solution_string").innerHTML.charAt(i - 1));
+        cell = $('#' + i + ' input');
+        value = $("#solution_string").text().charAt(i - 1);
+        cell.val(value);
+        console.log(cell.val(value));
     }
 }
